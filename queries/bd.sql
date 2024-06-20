e3d9d355480ccb2609b605b3543d429f6ceff15d
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-20 13:14:25 -0300

    inserindo varios inserts na mesma tabela

--- queries/bd.sql
+++ queries/bd.sql
@@ -41,3 +41,52 @@ CREATE TABLE TABELA_DE_VENDEDORES2 (
 
 -- DROP DA TABLE ACIMA
 drop table if exists TABELA_DE_VENDEDORES2;
+
+
+-- --- -- - -- -- -- - -- -- -- - -- - -- - - -- - -- -
+-- inserindo dados ao banco de dados tabela tbProduto
+-- vendo a inserção
+select * from tbproduto;
+
+-- inserindo os dados com values
+INSERT INTO tbproduto (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES (
+'1040107', 'Light - 350 ml - Melância',
+'Lata', '350 ml', 'Melância', 4.56);
+
+-- desafio da aula
+-- Vamos criar um vendedor na tabela de vendedores. A informação é a seguinte:
+INSERT tabela_de_vendedores (MATRICULA, NOME, PERCENTUAL_comissao)
+VALUES ('00233', 'João Geraldo da Fonseca', 0.10)
+('00235', 'MCláudia Morais', 0.8),
+('00236', 'Márcio Almeida Silva', 0.8);
+
+INSERT INTO tbproduto (
+PRODUTO, 
+NOME, 
+EMBALAGEM, 
+TAMANHO, 
+SABOR, 
+PRECO_LISTA) VALUES (
+'1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01);
+
+INSERT INTO tbproduto (
+PRODUTO, 
+NOME, 
+EMBALAGEM, 
+TAMANHO, 
+SABOR, 
+PRECO_LISTA) VALUES (
+'1000889', 'Sabor da Montanha - 700 ml - Uva', 'Garrafa', '700 ml', 'Uva', 6.31);
+
+INSERT INTO tbproduto (
+PRODUTO, 
+NOME, 
+EMBALAGEM, 
+TAMANHO, 
+SABOR, 
+PRECO_LISTA) VALUES (
+'1004327', 'Videira do Campo - 1,5 Litros - Melancia', 'PET', '1,5 Litros', 'Melancia', 19.51);
+
+SELECT * FROM tbproduto;
