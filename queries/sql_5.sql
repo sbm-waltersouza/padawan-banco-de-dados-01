812b7b889443f1d28d9c79c7688d63eeeeca94b8
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-20 14:49:58 -0300

    filtros por menor, maior e diferete

--- queries/sql_5.sql
+++ queries/sql_5.sql
@@ -94,3 +94,34 @@ SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
 SELECT * FROM tbproduto WHERE SABOR = 'Cítricos';
 
 UPDATE tbproduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão';
+
+SELECT * FROM TABELA_DE_VENDEDORES WHERE NOME = 'Claudia Morais';
+
+SELECT * FROM tbcliente;
+
+SELECT * FROM tbcliente WHERE IDADE = 22;
+
+SELECT * FROM tbcliente WHERE IDADE > 22;
+
+SELECT * FROM tbcliente WHERE IDADE < 22;
+
+SELECT * FROM tbcliente WHERE IDADE <= 22;
+
+SELECT * FROM tbcliente WHERE IDADE <> 22;
+
+SELECT * FROM tbcliente WHERE NOME >= 'Fernando Cavalcante';
+
+SELECT * FROM tbcliente WHERE NOME <> 'Fernando Cavalcante';
+
+SELECT * FROM tbproduto;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA > 16.008;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA < 16.008;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA <> 16.008;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA BETWEEN 16.007 AND 16.009;
+
+SELECT * FROM TABELA_DE_VENDEDORES WHERE PERCENTUAL_COMISSAO > 0.10;
+
